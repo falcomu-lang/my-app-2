9006c8b203df8a2af9d1c6ce9ded8fd25d3ee9cb
falcomu <falcomu@users.noreply.github.com>
2026-08-14 16:44:51 +0800

    Remove CMP OUT output-state validation

--- CameraCaptureApp/Services/Lsi8181MeterWheelService.cs
+++ CameraCaptureApp/Services/Lsi8181MeterWheelService.cs
@@ -53,7 +53,6 @@ namespace CameraCaptureApp.Services
 
             status = Lsi8181Native.LSI8181_toggle_preset(cardId, Lsi8181Native.CmpOutEnabled);
             EnsureSuccess(status, "Enable CMP OUT");
-            EnsureCmpOutEnabled(cardId);
 
             status = Lsi8181Native.LSI8181_counter_start(cardId, Lsi8181Native.CounterCompare);
             EnsureSuccess(status, "Start encoder counter with compare output");
@@ -121,7 +120,6 @@ namespace CameraCaptureApp.Services
 
             status = Lsi8181Native.LSI8181_toggle_preset(_cardId, Lsi8181Native.CmpOutEnabled);
             EnsureSuccess(status, "Enable CMP OUT");
-            EnsureCmpOutEnabled(_cardId);
         }
 
         public void Close()
@@ -163,15 +161,5 @@ namespace CameraCaptureApp.Services
             }
         }
 
-        private static void EnsureCmpOutEnabled(byte cardId)
-        {
-            byte compareOut = 0;
-            var status = Lsi8181Native.LSI8181_CO_read(cardId, ref compareOut);
-            EnsureSuccess(status, "Read CMP OUT status");
-            if (compareOut != Lsi8181Native.CmpOutEnabled)
-            {
-                throw new InvalidOperationException("CMP OUT enable readback failed. Current state: " + compareOut);
-            }
-        }
     }
 }
