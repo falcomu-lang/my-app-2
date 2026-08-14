c8bd44f92b5d559dff6e144b9a74a5e1e8f183f1
falcomu <falcomu@users.noreply.github.com>
2026-08-14 11:27:49 +0800

    Guard LSI-8181 segment selections

--- Lsi8181Official/Segment_Form.vb
+++ Lsi8181Official/Segment_Form.vb
@@ -1,4 +1,5 @@
 Public Class Segment_Form
+    Private _initialized As Boolean
     Dim s_control(3) As ComboBox
     Dim s_start(3) As TextBox
     Dim s_stop(3) As TextBox
@@ -7,6 +8,8 @@ Public Class Segment_Form
         Read_parameter()
     End Sub
     Sub Initial()
+        If _initialized Then Return
+
         s_control(0) = Control_ComboBox_0
         s_control(1) = Control_ComboBox_1
         s_control(2) = Control_ComboBox_2
@@ -18,12 +21,14 @@ Public Class Segment_Form
         s_stop(0) = Stop_TextBox_0
         s_stop(1) = Stop_TextBox_1
         s_stop(2) = Stop_TextBox_2
+
+        _initialized = True
     End Sub
     Sub Apply()
         Dim i As Byte
 
         For i = 0 To 2
-            Status = LSI8181_segment_control_write(CardID, i, s_control(i).SelectedIndex)
+            Status = LSI8181_segment_control_write(CardID, i, GetSelectedIndex(s_control(i)))
             Status = LSI8181_cmp_segment_write(CardID, i, Val(s_start(i).Text), Val(s_stop(i).Text))
         Next
     End Sub
@@ -59,13 +64,25 @@ Public Class Segment_Form
 
     Private Sub Mask_ComboBox_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Mask_ComboBox.SelectedIndexChanged
         If OfficialSettingsStore.IsRestoring Then Return
-        Dim mode As Byte = Mask_ComboBox.SelectedIndex
+        Dim mode As Byte = GetSelectedIndex(Mask_ComboBox)
         Status = LSI8181_mask_off_write(CardID, mode)
     End Sub
 
     Public Sub ApplySavedSettingsToCard()
         Initial()
         Apply()
-        Status = LSI8181_mask_off_write(CardID, Mask_ComboBox.SelectedIndex)
+        Status = LSI8181_mask_off_write(CardID, GetSelectedIndex(Mask_ComboBox))
     End Sub
+
+    Private Function GetSelectedIndex(ByVal comboBox As ComboBox) As Byte
+        If comboBox.SelectedIndex < 0 Then
+            If comboBox.Items.Count > 0 Then
+                comboBox.SelectedIndex = 0
+            End If
+
+            Return 0
+        End If
+
+        Return CByte(comboBox.SelectedIndex)
+    End Function
 End Class
